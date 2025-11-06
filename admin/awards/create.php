4940066305dd3f04657650d8d02b5374903726d9
JosephAGallucci <josephagallucci38@gmail.com>
2025-11-05 22:57:17 -0500

    Create done, Awards.csv made...other things undone

--- admin/awards/create.php
+++ admin/awards/create.php
@@ -1,18 +1,22 @@
 <?php
     function createCSV($file, $year, $award)
     {
-        /*while (($record = fgetcsv($f))) {
-            foreach($record as $field) {
-                echo $field . " ";
-            }
-            echo "<br/>";
-        }*/
+        $f = fopen($file, "a");
+
+        //Reaches end of CSV before writing to a new entry
+        while (($record = fgetcsv($f)))
+        {
+            fgetcsv($f);
+        }
+        fputcsv($f,[$year, $award]);
+        fclose($f);
     }
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
+        <title>Create New Entry</title>
     </head>
 
     <body>
@@ -20,10 +24,15 @@
 
         <form method="POST">
             "Year, Award" format<br>
-            <input type="number"/>
-            <input type="text"/>
+            <input type="number" name="year"/>
+            <input type="text" name="award"/>
             <br>
             <input type="submit"/>
+
+            <?php 
+                if ($_POST['year'] != NULL && $_POST['award'] != NULL)
+                createCSV("./awards.csv", $_POST['year'], $_POST['award']); 
+            ?>
         </form>
     </body>
 </html>
